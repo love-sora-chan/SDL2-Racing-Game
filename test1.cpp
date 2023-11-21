8d5ff8a8285e64f61c87b9d2595bbdca693cb46b
love-sora-chan <benini.huang@gmail.com>
2023-11-21 16:19:37 +0800

    turotial_learning2

--- test1.cpp
+++ test1.cpp
@@ -1,33 +1,15 @@
-/*This source code copyrighted by Lazy Foo' Productions 2004-2023
-and may not be redistributed without written permission.*/
-
-//Using SDL, standard IO, and strings
 #include <SDL.h>
-#include <stdio.h>
-#include <string>
+#include <iostream>
 
-//Screen dimension constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+using namespace std;
 
-//Key press surfaces constants
-enum KeyPressSurfaces
-{
-	KEY_PRESS_SURFACE_DEFAULT,
-	KEY_PRESS_SURFACE_UP,
-	KEY_PRESS_SURFACE_DOWN,
-	KEY_PRESS_SURFACE_LEFT,
-	KEY_PRESS_SURFACE_RIGHT,
-	KEY_PRESS_SURFACE_TOTAL
-};
+const int screen_width = 1280;
+const int screen_height = 720;
 
-//Starts up SDL and creates window
 bool init();
 
-//Loads media
-bool loadMedia();
+bool loadmedia();
 
-//Frees media and shuts down SDL
 void close();
 
 //Loads individual image
@@ -59,7 +41,7 @@ bool init()
 	else
 	{
 		//Create window
-		gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+		gWindow = SDL_CreateWindow( "Test1", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
 		if( gWindow == NULL )
 		{
 			printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
@@ -75,62 +57,14 @@ bool init()
 	return success;
 }
 
-bool loadMedia()
-{
-	//Loading success flag
-	bool success = true;
-
-	//Load default surface
-	gKeyPressSurfaces[ KEY_PRESS_SURFACE_DEFAULT ] = loadSurface( "04_key_presses/press.bmp" );
-	if( gKeyPressSurfaces[ KEY_PRESS_SURFACE_DEFAULT ] == NULL )
-	{
-		printf( "Failed to load default image!\n" );
-		success = false;
-	}
-
-	//Load up surface
-	gKeyPressSurfaces[ KEY_PRESS_SURFACE_UP ] = loadSurface( "04_key_presses/up.bmp" );
-	if( gKeyPressSurfaces[ KEY_PRESS_SURFACE_UP ] == NULL )
-	{
-		printf( "Failed to load up image!\n" );
-		success = false;
-	}
-
-	//Load down surface
-	gKeyPressSurfaces[ KEY_PRESS_SURFACE_DOWN ] = loadSurface( "04_key_presses/down.bmp" );
-	if( gKeyPressSurfaces[ KEY_PRESS_SURFACE_DOWN ] == NULL )
-	{
-		printf( "Failed to load down image!\n" );
-		success = false;
-	}
-
-	//Load left surface
-	gKeyPressSurfaces[ KEY_PRESS_SURFACE_LEFT ] = loadSurface( "04_key_presses/left.bmp" );
-	if( gKeyPressSurfaces[ KEY_PRESS_SURFACE_LEFT ] == NULL )
-	{
-		printf( "Failed to load left image!\n" );
-		success = false;
-	}
-
-	//Load right surface
-	gKeyPressSurfaces[ KEY_PRESS_SURFACE_RIGHT ] = loadSurface( "04_key_presses/right.bmp" );
-	if( gKeyPressSurfaces[ KEY_PRESS_SURFACE_RIGHT ] == NULL )
-	{
-		printf( "Failed to load right image!\n" );
-		success = false;
-	}
-
-	return success;
-}
-
 void close()
 {
 	//Deallocate surfaces
-	for( int i = 0; i < KEY_PRESS_SURFACE_TOTAL; ++i )
+	/*for( int i = 0; i < KEY_PRESS_SURFACE_TOTAL; ++i )
 	{
 		SDL_FreeSurface( gKeyPressSurfaces[ i ] );
 		gKeyPressSurfaces[ i ] = NULL;
-	}
+	}*/
 
 	//Destroy window
 	SDL_DestroyWindow( gWindow );
@@ -138,18 +72,6 @@ void close()
 
 	//Quit SDL subsystems
 	SDL_Quit();
-}
-
-SDL_Surface* loadSurface( std::string path )
-{
-	//Load image at specified path
-	SDL_Surface* loadedSurface = SDL_LoadBMP( path.c_str() );
-	if( loadedSurface == NULL )
-	{
-		printf( "Unable to load image %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
-	}
-
-	return loadedSurface;
 }
 
 
