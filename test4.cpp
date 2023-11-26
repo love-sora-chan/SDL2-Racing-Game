7484932aef567bcb86f643d85c9c9926934b1b12
love-sora-chan <benini.huang@gmail.com>
2023-11-27 01:28:36 +0800

    test

--- test4.cpp
+++ test4.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 #include <SDL.h>
+//#include <SDL2_gfxPrimitives.h>
 #include <cmath>
 #include <algorithm>
+#include <vector>
+//#include <SDL2_gfx>
+/*extern "C" {
+    #include "src\gfx\SDL2_gfxPrimitives.c"
+}*/
+#include <SDL2_gfxPrimitives.h>
+
 
 const int SCREEN_WIDTH = 1024;
 const int SCREEN_HEIGHT = 768;
@@ -51,7 +59,7 @@ bool init(){
     }
     else{
         //Create window
-        window = SDL_CreateWindow( "test3", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+        window = SDL_CreateWindow( "test4", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
 		if( window == NULL )
 		{
 			printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
@@ -88,6 +96,41 @@ void draw_quad(SDL_Renderer * renderer, int x1, int y1, int w1, int x2, int y2, 
         std::swap(w1,w2);
     }
 
+    //else{
+        std::vector< SDL_Vertex > verts1 =
+        {
+            { SDL_FPoint{ x1-w1, y1 }, SDL_Color{ color.r, color.g, color.b, color.a }, SDL_FPoint{ 0 }, },
+            { SDL_FPoint{ x1+w1, y1 }, SDL_Color{ color.r, color.g, color.b, color.a }, SDL_FPoint{ 0 }, },
+            { SDL_FPoint{ x2-w2, y2 }, SDL_Color{ color.r, color.g, color.b, color.a }, SDL_FPoint{ 0 }, },
+        };
+        SDL_RenderGeometry(renderer, NULL,verts1.data(), verts1.size(), NULL,0 );
+        std::vector< SDL_Vertex > verts2 =
+        {
+            { SDL_FPoint{ x2-w2, y2 }, SDL_Color{ color.r, color.g, color.b, color.a }, SDL_FPoint{ 0 }, },
+            { SDL_FPoint{ x1+w1, y1 }, SDL_Color{ color.r, color.g, color.b, color.a }, SDL_FPoint{ 0 }, },
+            { SDL_FPoint{ x2+w2, y2 }, SDL_Color{ color.r, color.g, color.b, color.a }, SDL_FPoint{ 0 }, },
+        };
+        SDL_RenderGeometry(renderer, NULL,verts2.data(), verts2.size(), NULL,0 );
+
+
+    //}
+/*
+    std::vector< SDL_Vertex > verts =
+    {
+        { SDL_FPoint{ 100, 100 }, SDL_Color{ 255, 255, 255, 255 }, SDL_FPoint{ 0 }, },
+        { SDL_FPoint{ 450, 450 }, SDL_Color{ 255, 255, 255, 255 }, SDL_FPoint{ 0 }, },
+        { SDL_FPoint{ 300,  450}, SDL_Color{ 255, 255, 255, 255 }, SDL_FPoint{ 0 }, },
+        { SDL_FPoint{ 150, 150 }, SDL_Color{ 255, 255, 255, 255 }, SDL_FPoint{ 0 }, },
+    };
+    SDL_RenderGeometry(renderer, NULL,verts.data(), verts.size(), NULL,0 ); 
+*/
+    /*
+    if(y1>y2){
+        std::swap(y1,y2);
+        std::swap(x1,x2);
+        std::swap(w1,w2);
+    }
+
     SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
 
     if(y1==y2){
@@ -100,7 +143,7 @@ void draw_quad(SDL_Renderer * renderer, int x1, int y1, int w1, int x2, int y2, 
             SDL_RenderDrawLine(renderer, xmid - w , y, xmid + w, y);
         }        
     }
-
+    */
 }
 
 void draw_scene(SDL_Renderer * renderer, Line3D * lines, int camZ, int n, int N){
@@ -120,7 +163,11 @@ void draw_scene(SDL_Renderer * renderer, Line3D * lines, int camZ, int n, int N)
         draw_quad(renderer, 0, prev_line->screenY, SCREEN_WIDTH, 0,curr_line->screenY, SCREEN_WIDTH, grass);
         draw_quad(renderer, prev_line->screenX, prev_line->screenY, prev_line->width*1.2 , curr_line->screenX ,curr_line->screenY, curr_line->width*1.2 , rumble);
         draw_quad(renderer, prev_line->screenX, prev_line->screenY, prev_line->width , curr_line->screenX ,curr_line->screenY, curr_line->width , road);
-        draw_quad(renderer, 50,50,50,200,200,100,Light_Road);
+        //draw_quad(renderer, 50,50,50,200,200,100,Light_Road);
+
+    
+    
+        
         
     }
 }
@@ -131,7 +178,7 @@ void framerate_cap(Uint32 start, int fps){
     if( elapsed_time < ms_per_frame ){
         SDL_Delay( (int) (ms_per_frame - elapsed_time) );
     }
-    std::cout<<ms_per_frame<<' ';
+    //std::cout<<ms_per_frame<<' ';
 }
 int WinMain(){
 
@@ -149,8 +196,13 @@ int WinMain(){
             SDL_Event e;
 
             //Create Lines
-            int N = 1600,camZ = 1;
-
+            int N = 1600 ,camZ = 1;
+            Line3D * Line3DArray = new Line3D[N];
+            for(int i = 0 ; i < N ; i++){
+                Line3D line;
+                Line3DArray[i] = line;
+                Line3DArray[i].z = segement_length * i;
+            }
 
 
             //Clear renderer
@@ -163,12 +215,7 @@ int WinMain(){
                 Uint32 time_start = SDL_GetTicks();
 
 
-                Line3D * Line3DArray = new Line3D[N];
-                for(int i = 0 ; i < N ; i++){
-                    Line3D line;
-                    Line3DArray[i] = line;
-                    Line3DArray[i].z = segement_length * i;
-                }
+
 
 
                 //Handle event on queue
@@ -180,11 +227,11 @@ int WinMain(){
                         switch( e.key.keysym.sym )
 						{
 							case SDLK_UP:
-                                camZ+=200;
+                                camZ+=500;
 							break;
 
 							case SDLK_DOWN:
-                                camZ-=200;
+                                camZ-=500;
 							break;
 
 							case SDLK_LEFT:
@@ -224,9 +271,10 @@ int WinMain(){
                 framerate_cap(time_start, 1000);
                 //std::cout<<"haha\n";
 
-                delete [] Line3DArray;
-                //delete Line3DArray;
+                
             }
+            delete [] Line3DArray;
+            //delete Line3DArray;
             
         }
     }
